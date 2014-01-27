11b75a25f12c02ce3cb2cee9b85bfcbac0580c3c
Mike Claussen <Mike_Claussen@symantec.com>
2014-01-27 15:25:21 -0800

    adding kickstart script with updates

--- kickstart.puppetmaster.sh
+++ kickstart.puppetmaster.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-yum  -y install ruby
+
+# Let's install some tools that we will need
+yum -y install dos2unix git wget ruby
 
 #install the proper repos
 rpm -ivh http://yum.puppetlabs.com/el/6/products/x86_64/puppetlabs-release-6-7.noarch.rpm
-
 
 #install the puppetmaster
 yum -y install puppet-server
@@ -11,7 +12,9 @@ yum -y install puppet-server
 #install puppet helpers
 gem install puppet-lint
 gem install rspec-puppet
-gem install puppet-catalog-test
+# We don't want to use this one, since it will 'gem install puppet'
+#gem install puppet-catalog-test
+mkdir ~/github && cd ~/github && git clone https://github.com/claussen/puppet-catalog-test.git  
+#clone Mike’s version
+cd ~/github/puppet-catalog-test && gem install ~/github/puppet-catalog-test-0.3.1.gem 
 
-yum -y install dos2unix
-yum -y install git
